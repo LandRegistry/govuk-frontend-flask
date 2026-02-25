f9316b25aaa927d95e0e7d46d5a23c055465b9fb
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2026-02-25 09:42:22 +0000

    organise config

--- config.py
+++ config.py
@@ -4,10 +4,29 @@ import redis
 
 
 class Config(object):
+    # Flask
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SECURE = True
+    SESSION_TYPE = "redis"
+
+    # Backing services
+    RATELIMIT_HEADERS_ENABLED = True
+    RATELIMIT_STORAGE_URI = os.environ.get("VALKEY_URL")
+    SESSION_REDIS = redis.from_url(os.environ.get("VALKEY_URL"))
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    # Service metadata
     CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL")
     CONTACT_PHONE = os.environ.get("CONTACT_PHONE")
     DEPARTMENT_NAME = os.environ.get("DEPARTMENT_NAME")
     DEPARTMENT_URL = os.environ.get("DEPARTMENT_URL")
+    SERVICE_NAME = os.environ.get("SERVICE_NAME")
+    SERVICE_PHASE = os.environ.get("SERVICE_PHASE")
+    SERVICE_URL = os.environ.get("SERVICE_URL")
+
+    # GOV.UK One Login
     ONE_LOGIN_ACCESS_TOKEN_URL = os.environ.get("ONE_LOGIN_ACCESS_TOKEN_URL")
     ONE_LOGIN_AUTHORIZE_URL = os.environ.get("ONE_LOGIN_AUTHORIZE_URL")
     ONE_LOGIN_CLIENT_ID = os.environ.get("ONE_LOGIN_CLIENT_ID")
@@ -16,18 +35,6 @@ class Config(object):
     ONE_LOGIN_PRIVATE_KEY_PATH = os.environ.get("ONE_LOGIN_PRIVATE_KEY_PATH")
     ONE_LOGIN_PUBLIC_TOKEN_URL = os.environ.get("ONE_LOGIN_PUBLIC_TOKEN_URL")
     ONE_LOGIN_USERINFO_URL = os.environ.get("ONE_LOGIN_USERINFO_URL")
-    RATELIMIT_HEADERS_ENABLED = True
-    RATELIMIT_STORAGE_URI = os.environ.get("VALKEY_URL")
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SERVICE_NAME = os.environ.get("SERVICE_NAME")
-    SERVICE_PHASE = os.environ.get("SERVICE_PHASE")
-    SERVICE_URL = os.environ.get("SERVICE_URL")
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = "Lax"
-    SESSION_COOKIE_SECURE = True
-    SESSION_REDIS = redis.from_url(os.environ.get("VALKEY_URL"))
-    SESSION_TYPE = "redis"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestConfig(Config):
