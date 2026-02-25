7c241b82af68613d54875eb5ce4dc369220a68f1
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2026-02-25 14:30:19 +0000

    dont need a 401 page

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -7,7 +7,6 @@ from authlib.jose import jwt
 from authlib.oauth2.rfc7523 import PrivateKeyJWT
 from authlib.oidc.core import CodeIDToken
 from flask import current_app, redirect, render_template, request, session, url_for
-from werkzeug.exceptions import Unauthorized
 
 from app.auth import bp
 
@@ -88,7 +87,7 @@ def user():
     userinfo = session.get("userinfo")
 
     if not userinfo:
-        raise Unauthorized
+        return redirect(url_for("auth.login"))
 
     return render_template("user.html", userinfo=userinfo)
 
