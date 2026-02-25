987ddba232d1f96e6ec3490c7c0fa5b8805ec6c5
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2026-02-25 11:08:40 +0000

    consistent user object naming

--- app/main/routes.py
+++ app/main/routes.py
@@ -20,8 +20,8 @@ from app.main.forms import CookiesForm
 @bp.route("/", methods=["GET"])
 def index() -> str:
     """Render the index page."""
-    user = session.get("userinfo")
-    return render_template("index.html", user=user)
+    userinfo = session.get("userinfo")
+    return render_template("index.html", userinfo=userinfo)
 
 
 @bp.route("/accessibility", methods=["GET"])
